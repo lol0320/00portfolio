9584fc07d9bf779aeb4df91acd859d60dff828dd
lol0320 <lol0320@naver.com>
2020-11-05 18:14:30 +0900

    2020-11-05

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,31 @@
-var a = '이예림'
+scrollVert();
+var scrollLeft=0;
+function scrollVert() {
+  $('html, body, *').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollTop -= (delta * 40);
+    e.preventDefault();
+    setTimeout(function() {
+      if ($(window).scrollTop() + $(window).height() == $(document).height())
+        scrollHoriz();
+    }, 0)
+  });
+}
+function scrollHoriz() {
+  $('html, body, *').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollLeft -= (delta * 40);
+    e.preventDefault();
+    scrollLeft=this.scrollLeft
+    setTimeout(function() {
+      if (scrollLeft == 0) scrollVert();
+    }, 0)
+  });
+}
 
-console.log('a');
+
+	$('html').each(function (){
+  	var _width = $(this).width(),
+    		_length = $(this).length
+      // _index = $(this).find('.on').index();
+                
+    $('.header-wrapper').scrollLeft((_width/_length) * _index);
+  });
