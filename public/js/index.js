71f14931e953ed41c7dbce5a16d09acd9022bdae
booldook <booldook@gmail.com>
2020-11-06 18:19:56 +0900

    booldook

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,50 @@
- scrollVert();
-var scrollLeft=0;
-function scrollVert() {
-  $('header-wrapper, project, *').off('mousewheel').mousewheel(function(e, delta) {
-    this.scrollTop -= (delta * 40);
-    e.preventDefault();
-    setTimeout(function() {
-      if ($(window).scrollTop() + $(window).height() == $(document).height())
-        scrollHoriz();
-    }, 0)
-  });
+
+var now = 0;
+
+function onWheel(e) {
+	$(this).off('wheel');
+	//e.preventDefault();
+	//e.originalEvent.preventDefault();
+	var delta = e.originalEvent.deltaY;
+	/*
+	for(var i=4; i>0; i--) {
+		if(x > wid * (i - 1)) {
+			now = i - 1;
+			break;
+		}
+	}
+	*/
+	console.log(now);
+	if(delta > 0 && now < 3) {
+		ani(delta);
+	}
+	if(delta < 0 && now > 0) {
+		ani(delta);
+	}
 }
 
+
+$(window).on('wheel', onWheel);
+
+
+
+
+
+function ani(delta) {
+	var wid = $(window).width();
+	var left = 0;
+	if(delta > 0){
+		left = Number($(".main-wrapper").css("left").replace("px", "")) + wid;
+	}
+	else {
+		left = Number($(".main-wrapper").css("left").replace("px", "")) - wid;
+	}
+	$(".main-wrapper").stop().animate({"left": -left+ "px"}, 300, function(){
+		$(window).on('wheel', onWheel);
+	})
+}
+
+/*
 function scrollHoriz() {
   $('html, body, *').off('mousewheel').mousewheel(function(e, delta) {
     this.scrollLeft -= (delta * 40);
@@ -22,4 +56,4 @@ function scrollHoriz() {
   });
 } 
 
-
+*/
