4e79d3a6216722c711453f1d5ef07987069a3a20
lol0320 <lol0320@naver.com>
2020-11-11 18:20:32 +0900

    집에가서해야됨

--- public/js/index.js
+++ public/js/index.js
@@ -1,48 +1,50 @@
 
 var now = 0;
+var delta;
+var wid = $(window).width();
+var scroll = 60;
+var hancan = 1/(wid/scroll);
 
 function onWheel(e) {
-	$(this).off('wheel');
-	//e.preventDefault();
-	//e.originalEvent.preventDefault();
-	var delta = e.originalEvent.deltaY;
-	/*
-	for(var i=4; i>0; i--) {
-		if(x > wid * (i - 1)) {
-			now = i - 1;
-			break;
-		}
+	delta = e.originalEvent.deltaY;
+	if(delta > 0 && now < 1) {
+		now++;
+		ani('fast');
 	}
-	*/
-	console.log(now);
-	if(delta > 0 && now < 3) {
-		ani(delta);
+	else if(delta < 0 && now <= 1 && now > 0) {
+		now--;
+		ani('fast');
 	}
-	if(delta < 0 && now > 0) {
-		ani(delta);
+	else if(delta > 0 && now >= 1  && now <= 2) {
+		now=now+hancan
+		deltaX +=  scroll;
+		ani('slow');
+	}
+	else if(delta < 0 && now > 1 && now <= 2) {
+		now=now-hancan
+		deltaX -= scroll;
+		ani('slow');
+	}
+	else if(delta > 0 && now > 1){
+		now= Math.floor(now +1);
+		ani('fast');
+	}
+	else if(delta > 0 && now > 1){
+		now--;
+		ani('fast');
 	}
 }
 
+var deltaX = 0;
+function ani(speed) {
+	
+	console.log(delta,deltaX,now);
+	if(speed == 'fast')	$(".main-wrapper").stop().animate({"left": -(now*wid)+ "px"} , 800);
+	if(speed == 'slow')	{
+		$(".main-wrapper").stop().css({"left": -wid-deltaX+ "px"});
+}}
 
 $(window).on('wheel', onWheel);
-
-
-
-
-
-function ani(delta) {
-	var wid = $(window).width();
-	var left = 0;
-	if(delta > 0){
-		left = Number($(".main-wrapper").css("left").replace("px", "")) + wid;
-	}
-	else {
-		left = Number($(".main-wrapper").css("left").replace("px", "")) - wid;
-	}
-	$(".main-wrapper").stop().animate({"left": -left+ "px"}, 300, function(){
-		$(window).on('wheel', onWheel);
-	})
-}
 
 /*
 function scrollHoriz() {
