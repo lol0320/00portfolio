8a40ea23538abb9d0f9993223215d5c49b83e28d
lol0320 <lol0320@naver.com>
2020-11-16 18:20:48 +0900

    2020-11-16

---
+++ typing_project/js/script.js
@@ -0,0 +1,35 @@
+var typingBool = false; 
+var typingIdx=0; 
+var liIndex = 0;
+var liLength = $(".typing-txt>ul>li").length;
+
+
+var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+typingTxt=typingTxt.split(""); 
+if(typingBool==false){ 
+    typingBool=true; 
+    var tyInt = setInterval(typing,100); 
+} 
+     
+function typing(){ 
+  if(typingIdx<typingTxt.length){ 
+     $(".typing").append(typingTxt[typingIdx]); 
+     typingIdx++; 
+   } else{ 
+     if(liIndex>=liLength-1){
+       liIndex=0;
+     }else{
+       liIndex++; 
+     }
+     
+        typingIdx=0;
+        typingBool = false; 
+        typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+       
+         clearInterval(tyInt);
+         setTimeout(function(){
+            $(".typing").html('');
+           tyInt = setInterval(typing,100);
+         },1000);
+    } 
+}  
